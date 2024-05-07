cd947afaecd2c04e0e30f04e498d68d384f83fe5
leonel <leo87llanca@gmail.com>
2024-05-07 18:39:55 -0300

    fixed application properties

--- CarritoDeCompras/src/main/java/com/example/CarritoDeCompras/CarritoDeComprasApplication.java
+++ CarritoDeCompras/src/main/java/com/example/CarritoDeCompras/CarritoDeComprasApplication.java
@@ -1,11 +1,13 @@
 package com.example.CarritoDeCompras;
 
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class CarritoDeComprasApplication {
 
+	
 	public static void main(String[] args) {
 		SpringApplication.run(CarritoDeComprasApplication.class, args);
 	}
